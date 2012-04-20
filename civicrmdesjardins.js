9d2fa989b3c897da1bc582ffe65ad1e8fe8476bd
Mathieu Lutfy <mathieu@bidon.ca>
2012-04-20 15:54:16 -0400

    hide the CC type field, use only the card icons, and auto-detect card type using the first numbers of the card

--- civicrmdesjardins.js
+++ civicrmdesjardins.js
@@ -10,16 +10,22 @@
 
       $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-visa').click(function() {
         $('#crm-container .crm-contribution-main-form-block .credit_card_type-section #credit_card_type').val('Visa');
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section a').css('opacity', 0.4);
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-visa').css('opacity', 1);
         $('input#cvv2').val('000');
         return false;
       });
       $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-mc').click(function() {
         $('#crm-container .crm-contribution-main-form-block .credit_card_type-section #credit_card_type').val('MasterCard');
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section a').css('opacity', 0.4);
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-mc').css('opacity', 1);
         $('input#cvv2').val('000');
         return false;
       });
       $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-amex').click(function() {
         $('#crm-container .crm-contribution-main-form-block .credit_card_type-section #credit_card_type').val('Amex');
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section a').css('opacity', 0.4);
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-amex').css('opacity', 1);
         $('input#cvv2').val('0000');
         return false;
       });
@@ -30,6 +36,29 @@
           $('input#cvv2').val('0000');
         } else {
           $('input#cvv2').val('000');
+        }
+      });
+
+      // Hide the CC type field (redundant anyway) and select according to the number entered
+      $('input#credit_card_number').change(function() {
+        var ccnumtype = $(this).val().substr(0, 4);
+
+        // Semi-hide all images, we will un-hide the right one afterwards
+        $('#crm-container .crm-contribution-main-form-block .credit_card_type-section a').css('opacity', 0.4);
+        $('#credit_card_type').val('');
+
+        // https://en.wikipedia.org/wiki/Credit_card_numbers
+        if (ccnumtype.substr(0, 1) == '3') {
+          $('#credit_card_type').val('Amex');
+          $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-amex').css('opacity', 1);
+        }
+        else if (ccnumtype.substr(0, 2) >= '51' && ccnumtype.substr(0, 2) <= '55') {
+          $('#credit_card_type').val('MasterCard');
+          $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-mc').css('opacity', 1);
+        }
+        else if (ccnumtype.substr(0, 1) == '4') {
+          $('#credit_card_type').val('Visa');
+          $('#crm-container .crm-contribution-main-form-block .credit_card_type-section .civicrmdesjardins-cclogo-visa').css('opacity', 1);
         }
       });
     }
