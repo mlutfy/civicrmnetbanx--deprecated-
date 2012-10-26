66a1bbf5b1f9d2fabf0287da32c24777f0096d82
Mathieu Lutfy <mathieu@bidon.ca>
2012-10-26 11:03:18 -0400

    Netbanx supports cvv2, so remove old hacks.

--- civicrmdesjardins.js
+++ civicrmdesjardins.js
@@ -12,31 +12,19 @@
         cj('#crm-container .credit_card_type-section #credit_card_type').val('Visa');
         cj('#crm-container .credit_card_type-section a').css('opacity', 0.4);
         cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-visa').css('opacity', 1);
-        cj('input#cvv2').val('000');
         return false;
       });
       cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-mc').click(function() {
         cj('#crm-container .credit_card_type-section #credit_card_type').val('MasterCard');
         cj('#crm-container .credit_card_type-section a').css('opacity', 0.4);
         cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-mc').css('opacity', 1);
-        cj('input#cvv2').val('000');
         return false;
       });
       cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-amex').click(function() {
         cj('#crm-container .credit_card_type-section #credit_card_type').val('Amex');
         cj('#crm-container .credit_card_type-section a').css('opacity', 0.4);
         cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-amex').css('opacity', 1);
-        cj('input#cvv2').val('0000');
         return false;
-      });
-
-      cj('#crm-container .credit_card_type-section select#credit_card_type').change(function() {
-        var cctype = cj(this).val();
-        if (cctype == 'Amex') {
-          cj('input#cvv2').val('0000');
-        } else {
-          cj('input#cvv2').val('000');
-        }
       });
 
       // Hide the CC type field (redundant anyway) and select according to the number entered
@@ -51,17 +39,14 @@
         if (ccnumtype.substr(0, 1) == '3') {
           cj('#credit_card_type').val('Amex');
           cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-amex').css('opacity', 1);
-          cj('input#cvv2').val('0000');
         }
         else if (ccnumtype.substr(0, 2) >= '51' && ccnumtype.substr(0, 2) <= '55') {
           cj('#credit_card_type').val('MasterCard');
           cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-mc').css('opacity', 1);
-          cj('input#cvv2').val('000');
         }
         else if (ccnumtype.substr(0, 1) == '4') {
           cj('#credit_card_type').val('Visa');
           cj('#crm-container .credit_card_type-section .civicrmdesjardins-cclogo-visa').css('opacity', 1);
-          cj('input#cvv2').val('000');
         }
       });
     }
